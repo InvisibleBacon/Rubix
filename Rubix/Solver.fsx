d482f8d4a87786d9463e5fe44e7039e115789617
Mark Porter <mporter@swiftsoftware.com>
2014-07-11 14:12:44 -0400

    Added rendering one side

---
+++ Rubix/Solver.fsx
@@ -0,0 +1,36 @@
+﻿#r "../references/OpenTK.dll"
+#r "../references/OpenTK.GLControl.dll"
+#load "functional3d.fs"
+
+open Functional3D
+open System.Drawing
+
+type Cublet = 
+    | Green
+    | Yellow
+    | Red
+    | Orange
+    | Blue
+    | White
+
+let buildSolvedFace (c : Cublet) =
+   Array2D.init 3 3 (fun x y -> c)
+
+let flatten (A:'a[,]) = A |> Seq.cast<'a>
+
+let renderFace (face : Cublet [,]) =
+    face
+    |> Array2D.mapi (
+        fun x y color -> 
+            Fun.cube
+            |> Fun.color Color.Blue
+            |> Fun.scale(0.9, 0.9, 0.9)
+            |> Fun.translate (float x, float y, 0.0)
+    )
+    |> flatten
+    |> Seq.reduce ($)
+
+
+let test =
+    buildSolvedFace Cublet.Red
+    |> renderFace
