c9383557484f33ee4082a14f807d2f45beb4952c
Mark Porter <mporter@swiftsoftware.com>
2014-07-11 21:25:45 -0400

    rendering solved rubix cube

--- Rubix/Solver.fsx
+++ Rubix/Solver.fsx
@@ -5,7 +5,7 @@
 open Functional3D
 open System.Drawing
 
-type Cublet = 
+type cublet = 
     | Green
     | Yellow
     | Red
@@ -13,24 +13,65 @@ type Cublet =
     | Blue
     | White
 
-let buildSolvedFace (c : Cublet) =
+let getCubletColor = function
+    | Green -> Color.Green
+    | Yellow -> Color.Yellow
+    | Red -> Color.Red
+    | Orange -> Color.Orange
+    | Blue -> Color.Blue
+    | White -> Color.White
+
+let buildSolvedFace (c : cublet) =
    Array2D.init 3 3 (fun x y -> c)
 
 let flatten (A:'a[,]) = A |> Seq.cast<'a>
 
-let renderFace (face : Cublet [,]) =
+type cubeFace = cublet [,]
+
+let renderFace (face : cubeFace) =
     face
     |> Array2D.mapi (
-        fun x y color -> 
+        fun x y cublet -> 
             Fun.cube
-            |> Fun.color Color.Blue
+            |> Fun.color Color.Black
+            $
+            (Fun.cube
+                |> Fun.color (getCubletColor cublet)
+                |> Fun.scale(1.0, 1.0, 0.1)
+                |> Fun.translate (0.0, 0.0, 0.5))
             |> Fun.scale(0.9, 0.9, 0.9)
             |> Fun.translate (float x, float y, 0.0)
     )
     |> flatten
     |> Seq.reduce ($)
 
+type cube = {
+    front : cubeFace;
+    back : cubeFace;
+    left: cubeFace;
+    right: cubeFace;
+    top: cubeFace;
+    bottom: cubeFace;
+}
+
+let solved = {
+    front = (buildSolvedFace cublet.Red);
+    back = (buildSolvedFace cublet.Orange);
+    left = (buildSolvedFace cublet.Green);
+    right = (buildSolvedFace cublet.Blue);
+    top = (buildSolvedFace cublet.White);
+    bottom = (buildSolvedFace cublet.Yellow);
+}
+
+let renderCube (c : cube) =
+    renderFace c.front $
+    (renderFace c.left |> Fun.rotate (0.0, 270.0, 0.0) |> Fun.translate (0.0, 0.0, -2.0)) $
+    (renderFace c.right |> Fun.rotate (0.0, 90.0, 0.0) |> Fun.translate (2.0, 0.0, 0.0)) $
+    (renderFace c.back |> Fun.rotate (0.0, 180.0, 0.0) |> Fun.translate (2.0, 0.0, -2.0)) $
+    (renderFace c.top |> Fun.rotate (270.0, 0.0, 0.0) |> Fun.translate (0.0, 2.0, 0.0)) $
+    (renderFace c.bottom |> Fun.rotate (90.0, 0.0, 0.0) |> Fun.translate (0.0, 0.0, -2.0))
+    |> Fun.translate (-1.0, -1.0, 1.0)
 
 let test =
-    buildSolvedFace Cublet.Red
-    |> renderFace
+    solved
+    |> renderCube
